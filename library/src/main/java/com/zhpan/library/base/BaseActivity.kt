644da178c1dad2a764fa8239d3a8e416ff2a7370
zhpanvip <zhpanvip@outlook.com>
2020-07-05 18:34:29 +0800

    Add BaseFragment and BaseActivity

---
+++ library/src/main/java/com/zhpan/library/base/BaseActivity.kt
@@ -0,0 +1,43 @@
+package com.zhpan.library.base
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
+
+
+/**
+ * <pre>
+ *   Created by zhpan on 2020/7/5.
+ *   Description:
+ * </pre>
+ */
+abstract class BaseActivity<VM : BaseViewModel, VB : ViewDataBinding> : AppCompatActivity(),CoroutineScope by MainScope() {
+
+    protected val mBinding: VB by lazy {
+        DataBindingUtil.setContentView(this, getLayoutId()) as VB
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        mBinding.lifecycleOwner=this
+        onActivityCreated(savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cancel()
+        mBinding.unbind()
+    }
+
+   abstract fun onActivityCreated(savedInstanceState: Bundle?)
+
+    abstract fun getLayoutId(): Int
+
+    fun getViewModel(clazz: Class<VM>): VM = ViewModelProvider(this).get(clazz)
+}
---
+++ library/src/main/java/com/zhpan/library/base/BaseFragment.kt
@@ -0,0 +1,67 @@
+package com.zhpan.library.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
+
+
+/**
+ * <pre>
+ *   Created by zhpan on 2020/7/5.
+ *   Description:
+ * </pre>
+ */
+abstract class BaseFragment<VM : BaseViewModel, VB : ViewDataBinding> : Fragment(),
+    CoroutineScope by MainScope() {
+    protected var mBinding: VB? = null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        if (mBinding == null) {
+            mBinding = DataBindingUtil.inflate(inflater, getLayoutId(), container, false)
+            onViewInflate()
+        }
+
+        return if (mBinding != null) {
+            mBinding!!.root.apply {
+                (parent as? ViewGroup)?.removeView(this)
+            }
+        } else {
+            return super.onCreateView(inflater, container, savedInstanceState)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mBinding?.lifecycleOwner = this
+        initFragment()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cancel()
+        mBinding?.unbind()
+
+    }
+
+    fun getViewModel(clazz: Class<VM>): VM {
+        return ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(clazz)
+    }
+
+    abstract fun initFragment()
+
+    abstract fun onViewInflate()
+
+    abstract fun getLayoutId(): Int
+
+}
