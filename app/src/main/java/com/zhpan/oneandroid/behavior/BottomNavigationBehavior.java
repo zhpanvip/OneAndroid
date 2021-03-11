3183af5b27638247dac79412032f0bd7d2d9e3f7
zhpanvip <zhpanvip@outlook.com>
2021-03-11 18:42:05 +0800

    修改BottomNavigationBehavior

--- app/src/main/java/com/zhpan/oneandroid/behavior/BottomNavigationBehavior.java
+++ app/src/main/java/com/zhpan/oneandroid/behavior/BottomNavigationBehavior.java
@@ -4,16 +4,21 @@ import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Interpolator;
 
 import androidx.annotation.NonNull;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.core.view.ViewCompat;
+import androidx.interpolator.view.animation.FastOutSlowInInterpolator;
 
 /**
  * Created by chenxz on 2017/11/25.
  */
 public class BottomNavigationBehavior extends CoordinatorLayout.Behavior<View> {
-    private ObjectAnimator outAnimator, inAnimator;
+    private ObjectAnimator translateAnimator;
+    private static final long ANIMATE_SCROLL_DURATION = 250;
+    private static final Interpolator INTERPOLATOR = new FastOutSlowInInterpolator();
 
     public BottomNavigationBehavior(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -27,22 +32,28 @@ public class BottomNavigationBehavior extends CoordinatorLayout.Behavior<View> {
 
     @Override
     public void onNestedPreScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child, @NonNull View target, int dx, int dy, @NonNull int[] consumed, int type) {
-        if (dy > 0) {// 上滑隐藏
-            if (outAnimator == null) {
-                outAnimator = ObjectAnimator.ofFloat(child, "translationY", 0, child.getHeight());
-                outAnimator.setDuration(200);
+        if (translateAnimator == null) {
+            translateAnimator = ObjectAnimator.ofFloat(child, "translationY", 0, child.getHeight() + getMarginBottom(child));
+            translateAnimator.setDuration(ANIMATE_SCROLL_DURATION).setInterpolator(INTERPOLATOR);
+        }
+        if (dy > 0) {
+            // 开启隐藏动画
+            if (!translateAnimator.isRunning() && child.getTranslationY() <= 0) {
+                translateAnimator.start();
             }
-            if (!outAnimator.isRunning() && child.getTranslationY() <= 0) {
-                outAnimator.start();
-            }
-        } else if (dy < 0) {// 下滑显示
-            if (inAnimator == null) {
-                inAnimator = ObjectAnimator.ofFloat(child, "translationY", child.getHeight(), 0);
-                inAnimator.setDuration(200);
-            }
-            if (!inAnimator.isRunning() && child.getTranslationY() >= child.getHeight()) {
-                inAnimator.start();
+        } else if (dy < 0) {
+            // 开启显示动画
+            if (!translateAnimator.isRunning() && child.getTranslationY() >= child.getHeight()) {
+                translateAnimator.reverse();
             }
         }
+    }
+
+    private int getMarginBottom(View v) {
+        final ViewGroup.LayoutParams layoutParams = v.getLayoutParams();
+        if (layoutParams instanceof ViewGroup.MarginLayoutParams) {
+            return ((ViewGroup.MarginLayoutParams) layoutParams).bottomMargin;
+        }
+        return 0;
     }
 }
